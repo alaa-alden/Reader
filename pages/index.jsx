c4440f18e9c3ca591c8359e58c2504196b91c767
alaa-alden <alaa.alden.010@gmail.com>
2020-05-30 21:32:31 +0300

    some change

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Layout from '../components/layout'
+import Router from 'next/router'
 const index = () => {
+    React.useEffect(()=>{
+        Router.push('/text')
+    })
     return (
-        <Layout>
-           <h1>hello world!!!</h1>
-        </Layout>    
+        <div></div>
     )
 }
 
